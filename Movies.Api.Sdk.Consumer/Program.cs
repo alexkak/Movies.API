4ad246f93dcdda6711e8c21557ca7df77c0c5f41
a.kakiris <a.kakiris@noslg.com>
2023-10-02 10:06:00 +0300

    Creating the SDK project and Introducing Refit

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Movies.Api.Sdk;
+using Refit;
+using System.Text.Json;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:44318");
+
+var movie = await moviesApi.GetMoviesAsync("d1b965ee-0a07-45f7-b809-5062b2097126");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
